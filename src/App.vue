1efd2aaeea35e89623c8e77fd4bcb324f69de521
Cyan Yan <cyan_cg@qq.com>
2023-11-11 16:32:03 +0800

    Add main page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,57 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/protein_turnover">Home</router-link> | 
-      <router-link to="/protein_turnover/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <q-layout view="lHh lpR fFf">
+
+    <q-header elevated class="bg-dark text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="/atom.white.svg">
+          </q-avatar>
+          Protein Turnover
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/protein_turnover" label="Home" />
+        <q-route-tab to="/protein_turnover/about" label="About" />
+        <!-- <q-route-tab to="/page3" label="Page Three" /> -->
+      </q-tabs>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
