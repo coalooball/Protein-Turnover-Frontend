e1ae0d7a1e855aaccdee29b9459b3a2ee77c394a
Cyan Yan <cyan_cg@qq.com>
2023-11-11 15:33:53 +0800

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <template>
-  <div>
-    <p>{{ message }}</p>
+  <div id="app">
+    <nav>
+      <router-link to="/protein_turnover">Home</router-link> | 
+      <router-link to="/protein_turnover/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-
-const message = ref('');
-
-const fetchData = async () => {
-  try {
-    const response = await fetch('/api/data');
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    message.value = data.message;
-  } catch (error) {
-    console.error('Fetch error:', error);
-  }
-};
-
-fetchData();
-</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>{{ message }}</p>
+    </div>
+</template>
+  
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('');
+
+const fetchData = async () => {
+    try {
+        const response = await fetch('/api/data');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        message.value = data.message;
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+};
+
+fetchData();
+</script>
+  
