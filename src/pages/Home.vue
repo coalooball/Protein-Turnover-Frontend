7d961dbfdd228e98ae95b0c7adf75c74e982d5ae
Cyan Yan <cyan_cg@qq.com>
2023-11-11 21:45:08 +0800

    Amend

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,11 @@
 <template>
     <q-expansion-item v-model="expanded" dense dense-toggle expand-separator label="Host Information">
-        <q-card v-if="!host_info_loading" class="bg-grey-5">
+        <q-card v-if="host_info_loading_error !== ''" class="bg-grey-5">
+            <q-card-section>
+                <p class="text-red"> {{ host_info_loading_error }} </p>
+            </q-card-section>
+        </q-card>
+        <q-card v-else-if="!host_info_loading" class="bg-grey-5">
             <q-card-section>
                 <div class="row" v-for="(info_value, info_key) in message" :key="info_key">
                     <div class="col-3">
@@ -27,12 +32,12 @@ import { ref } from 'vue';
 const message = ref([]);
 const expanded = ref(true);
 const host_info_loading = ref(true);
+const host_info_loading_error = ref('');
 
 const fetchData = async () => {
+    host_info_loading.value = true;
     try {
-        host_info_loading.value = true;
         const response = await fetch('/api/host_informations');
-        host_info_loading.value = false
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
@@ -40,7 +45,10 @@ const fetchData = async () => {
         message.value = data;
     } catch (error) {
         console.error('Fetch error:', error);
+        host_info_loading_error.value = String(error);
     }
+    host_info_loading.value = false
+    console.log(host_info_loading_error.value)
 };
 
 fetchData();
