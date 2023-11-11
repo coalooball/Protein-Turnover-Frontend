7d41fd3adae72024561391264b6192e7900766fd
Cyan Yan <cyan_cg@qq.com>
2023-11-11 22:44:48 +0800

    Amend

--- src/pages/CreateJob.vue
+++ src/pages/CreateJob.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
-        CJ
+        <q-card>
+            <div class="row">
+                <div class="col-6">
+                    <q-card-section>
+                        <q-input v-model="job_name" label="job name" placeholder="Please enter the task name" :dense="dense" />
+                    </q-card-section>
+                </div>
+            </div>
+        </q-card>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+let job_name = ref('');
+let dense = ref(false);
+</script>
