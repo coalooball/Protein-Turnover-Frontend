6c36ea8e3c9491d514e162432b944f914f327240
Cyan Yan <cyan_cg@qq.com>
2023-11-11 17:46:06 +0800

    Add host informations

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,27 +2,33 @@
     <q-expansion-item dense dense-toggle expand-separator label="Host Information">
         <q-card class="bg-grey-5">
             <q-card-section>
-                <div>
-                    <p>{{ message }}</p>
+                <div class="row" v-for="(info_value, info_key) in message" :key="info_key">
+                    <div class="col-3">
+                        {{ info_value.key }}
+                    </div>
+                    <div class="col-9">
+                        {{ info_value.value }}
+                    </div>
                 </div>
             </q-card-section>
         </q-card>
     </q-expansion-item>
 </template>
   
+  
 <script setup>
 import { ref } from 'vue';
 
-const message = ref('');
+const message = ref([]);
 
 const fetchData = async () => {
     try {
-        const response = await fetch('/api/data');
+        const response = await fetch('/api/host_informations');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        message.value = data.message;
+        message.value = data;
     } catch (error) {
         console.error('Fetch error:', error);
     }
