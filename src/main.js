abfd1f2b7d2e5eb416e1da55d7c8c488034af87b
Cyan Yan <cyan_cg@qq.com>
2023-11-11 16:02:06 +0800

    Add Quasar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+// https://quasar.dev/start/vite-plugin
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
 
 createApp(App)
     .use(router)
+    .use(Quasar)
     .mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,14 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/protein_turnover", 
+  base: "/protein_turnover",
   plugins: [
     vue(),
+    // @quasar/plugin-vite options list:
+    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
   ],
   resolve: {
     alias: {
