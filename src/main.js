13d96747482295469e54dfbc762a4809972df06c
Cyan Yan <cyan_cg@qq.com>
2023-11-28 16:02:04 +0800

    Add clickhouse connection-related operations

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 import router from './router';
 // https://quasar.dev/start/vite-plugin
 import { Quasar } from 'quasar'
+import { Notify } from "quasar";
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 
 
 createApp(App)
     .use(router)
-    .use(Quasar)
+    // https://stackoverflow.com/questions/67861771/notifications-failing-at-notify
+    .use(Quasar, {
+        plugins: {
+            Notify
+        }
+    })
     .mount('#app');
--- src/sidebar/PreparationSidebar.vue
+++ src/sidebar/PreparationSidebar.vue
@@ -2,52 +2,93 @@
     <div class="row justify-center">
         Preparation Configuration
         <q-card flat bordered class="bg-grey-10">
-            <q-input v-model="host" label="Host" :dense="true" label-color="white" color="teal" input-class="text-white" />
-            <q-input v-model="port" label="Port" :dense="true" label-color="white" color="teal" input-class="text-white" />
-            <q-input v-model="username" label="Username" :dense="true" label-color="white" color="teal"
-                input-class="text-white" />
-            <q-input v-model="password" label="Password" :dense="true" label-color="white" color="teal"
-                input-class="text-white" />
-            <q-btn color="primary" @click="sendData">Ping</q-btn>
+            <q-input v-model="host" label="Host" :dense="true" label-color="white" color="teal" input-class="text-white" dark />
+            <q-input v-model="port" label="Port" :dense="true" label-color="white" color="teal" input-class="text-white" dark />
+            <q-input v-model="username" label="Username" :dense="true" label-color="white" color="teal" dark input-class="text-white" />
+            <q-input v-model="password" label="Password" :dense="true" label-color="white" color="teal" dark input-class="text-white" />
+            <q-space />
+            <div v-if="!is_connected">
+                <q-btn color="primary" @click="sendData">
+                    Connect
+                    <q-tooltip anchor="bottom middle" self="top middle">
+                        Click to connect clickhouse server
+                    </q-tooltip>
+                </q-btn>
+            </div>
+            <div v-else>
+                <q-btn color="positive" @click="sendData">
+                    Ping
+                    <q-tooltip anchor="bottom middle" self="top middle">
+                        Click to ping clickhouse server
+                    </q-tooltip>
+                </q-btn>
+            </div>
         </q-card>
     </div>
 </template>
   
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
+import { useQuasar } from "quasar";
 
-let host = ref('');
-let port = ref('');
-let username = ref('');
-let password = ref('');
+const $q = useQuasar();
+let host = ref("127.0.0.1");
+let port = ref("8123");
+let username = ref("");
+let password = ref("");
+let is_connected = ref(false);
 
 const sendData = () => {
     const data = {
         host: host.value,
         port: port.value,
         username: username.value,
-        password: password.value
+        password: password.value,
     };
 
-    fetch('/api/test_clickhouse_connection', {
-        method: 'POST',
+    fetch("/api/test_clickhouse_connection", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
     })
-        .then(response => {
+        .then((response) => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error("Network response was not ok");
             }
             return response.json();
         })
-        .then(data => {
+        .then((data) => {
             console.log(data);
+            is_connected.value = data;
+            console.log($q)
+            if (data) {
+                showNotifSucess();
+            } else {
+                showNotifFailed();
+            }
         })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
+        .catch((error) => {
+            console.error(
+                "There was a problem with the fetch operation:",
+                error
+            );
         });
 };
+
+function showNotifSucess() {
+    $q.notify({
+        message: "Connected!",
+        color: "positive",
+    });
+}
+
+function showNotifFailed() {
+    $q.notify({
+        message: "Failed to connect clickhouse!",
+        color: "negative",
+    });
+}
 </script>
   
