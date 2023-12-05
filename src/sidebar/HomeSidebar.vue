2a818bedffd8ed53468f16f47a0ee5693177f820
Cyan Yan <cyan_cg@qq.com>
2023-12-05 21:47:21 +0800

    Modify the sidebar of Home

--- src/sidebar/HomeSidebar.vue
+++ src/sidebar/HomeSidebar.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="row justify-center">
-        HOME
+        <q-chip dense square label="HOME" />
     </div>
+    <q-card flat bordered class="bg-grey-10 text-white">
+        <q-card-section>
+            <span style="font-size: 2em;">Protein Turnover</span> is a protein analysis software that identifies peptide
+            segments meeting certain criteria in the pepxml file based on mass spectrometry data in the mzML format.
+        </q-card-section>
+    </q-card>
 </template>
